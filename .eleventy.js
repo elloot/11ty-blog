17e80532e851e7eb390486f844316d67c110b641
elloot <elliot.duchek@gmail.com>
2021-09-21 14:04:41 +0200

    ✨ add image shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,33 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
+const Image = require('@11ty/eleventy-img');
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [400, null],
+    formats: ['webp', 'jpeg'],
+    outputDir: './_site/img'
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes: sizes || '100%',
+    loading: 'lazy',
+    decoding: 'async'
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
+
   eleventyConfig.addWatchTarget('src/scss');
+
+  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
+
   return {
     dir: {
       input: 'src'
