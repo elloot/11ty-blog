bf45ff32b436dc5027137124e7de425cc3cfa740
elloot <elliot.duchek@gmail.com>
2021-09-21 12:07:57 +0200

    ✨ change metadata to be js file

---
+++ src/_data/meta.js
@@ -0,0 +1,12 @@
+module.exports = {
+  url: process.env.URL || 'https://localhost:8080',
+  title: 'Nice cool blog',
+  subtitle: 'Blog',
+  description: 'This is a blog',
+  author: {
+    name: 'Elliot Duchek',
+    email: 'me@elloot.com',
+    github: 'elloot'
+  },
+  updated: Date.now()
+};
