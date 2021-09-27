41d07ef59ececedcc1996dd30af77e45457f3f26
elloot <54401777+elloot@users.noreply.github.com>
2021-09-27 17:16:48 +0200

    ✨ add filter for decreasing heading sizes in HTML template string 💩 ✨ add filter for getting readme of repo, doesn't work

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const Image = require('@11ty/eleventy-img');
+const Cache = require('@11ty/eleventy-cache-assets');
+const htmlToAbsoluteUrls = require('@11ty/eleventy-plugin-rss/src/htmlToAbsoluteUrls');
 
 async function imageShortcode(src, alt, sizes) {
   let metadata = await Image(src, {
@@ -29,6 +31,40 @@ function sortByDate(arr, dateName) {
   return arr;
 }
 
+function decreaseHeadingSizes(htmlTemplate) {
+  let newTemplate = htmlTemplate;
+  for (let i = 6; i > 0; i--) {
+    let hToReplace = 'h' + i;
+    try {
+      newTemplate = newTemplate.replaceAll(hToReplace, 'h' + (i + 1));
+    } catch (e) {
+      continue;
+    }
+  }
+  return newTemplate;
+}
+
+async function getRepoReadme(repo) {
+  try {
+    const readme = Cache(
+      `https://raw.githubusercontent.com/${repo.full_name}/main/README.md`,
+      {
+        duration: '12h',
+        type: 'text',
+        fethOptions: {
+          headers: {
+            authorization: process.env.GITHUB_TOKEN
+          }
+        }
+      }
+    );
+    return readme;
+  } catch (e) {
+    console.log(e);
+    return '';
+  }
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
@@ -40,6 +76,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('getLatestPost', (arr) => {
     return sortByDate(arr, 'date')[0];
   });
+
+  eleventyConfig.addFilter('decreaseHeadingSizes', decreaseHeadingSizes);
+
+  eleventyConfig.addFilter('getRepoReadme', getRepoReadme);
 
   eleventyConfig.addFilter('getLatestRepo', (arr) => {
     arr.forEach((repo) => {
