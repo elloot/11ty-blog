cd3a035ef938af89381883f1b63e40ebb019241a
elloot <54401777+elloot@users.noreply.github.com>
2021-09-28 12:42:09 +0200

    🔧 add terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+import { terser } from 'rollup-plugin-terser';
+
 export default {
   input: 'src/js/main.js',
   output: {
     file: '_site/js/bundle.js',
     format: 'iife'
-  }
+  },
+  plugins: [terser()]
 };
